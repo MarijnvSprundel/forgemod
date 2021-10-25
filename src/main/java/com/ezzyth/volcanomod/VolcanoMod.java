322e34c856f629f4d59a882328b9399b955665b4
MarijnvSprundel <71122319+MarijnvSprundel@users.noreply.github.com>
2021-10-25 15:39:11 +0200

    Added items and blocks using a tutorial, expect this to get deleted for other volcanic related items

--- src/main/java/com/ezzyth/volcanomod/VolcanoMod.java
+++ src/main/java/com/ezzyth/volcanomod/VolcanoMod.java
@@ -1,35 +1,29 @@
-package com.ezzyth.volcano;
+package com.ezzyth.volcanomod;
 
-import net.minecraft.world.level.block.Block;
+import com.ezzyth.volcanomod.block.ModBlocks;
+import com.ezzyth.volcanomod.item.ModItems;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
-import net.minecraftforge.fmlserverevents.FMLServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(VolcanoMod.MOD_ID)
 public class VolcanoMod
 {
-    public static final String MOD_ID = "volcano";
+    public static final String MOD_ID = "volcanomod";
     // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
 
     public VolcanoMod() {
         IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
         eventBus.addListener(this::setup);
-
+        ModItems.register(eventBus);
+        ModBlocks.register(eventBus);
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
     }
